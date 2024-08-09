89ed3cdb7d15008c1361eddd2d8824d752da1f3a
MixterOne <matheusisaias879@gmail.com>
2024-08-09 10:52:15 -0300

    alterando codigo

--- src/site/Components/site.jsx
+++ src/site/Components/site.jsx
@@ -19,7 +19,7 @@ function Site(){
           <Route path="/sobre" element={<Sobre/>} />
           <Route path="/projetos" element={<Projetos/>} />
           <Route path="/relatorio" element={<Relatorio/>} />
-          <Route path="/constato" element={<Contato/>} />
+          <Route path="/contato" element={<Contato/>} />
           <Route path='/*' element={<h1>Olá, tudo bem, como vai...</h1>} />
         </Routes>
         <Footer/>
