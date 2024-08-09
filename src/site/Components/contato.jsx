fc3df26caf7304491189efd7c91e25a8cee8b690
apriginh0 <apriginh0@hotmail.com>
2024-08-09 14:40:43 -0300

    Pequena mudanca em Conato

--- src/site/Components/contato.jsx
+++ src/site/Components/contato.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 
 function Contato(){
-    return <section id="preco">
-        <div className="container">
-
-            <div className="row text-center">
-
-                    </div>
-                </div>
-
-
-                    </section>;
+    return <section id="contato">
+        <div className="">
+            <div className="">
+                <h1>Contato</h1>
+            </div>
+        </div>
+    </section>;
   }
 
 export default Contato;
