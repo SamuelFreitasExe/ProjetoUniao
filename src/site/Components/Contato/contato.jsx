8276ae93fadfe38e4ff5107c0344bf18e99407f7
apriginh0 <apriginh0@hotmail.com>
2024-08-09 18:13:45 -0300

    Criacao de pastas

---
+++ src/site/Components/Contato/contato.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Contato(){
+    return <section id="preco">
+         <div className="container">
+             <div className="row text-center">
+                <h1>Contato</h1>
+                <p>Página em desenvolvimento</p>
+             </div>
+         </div>
+
+
+        </section>;
+  }
+
+export default Contato;
