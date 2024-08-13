c61ce613da6574c299be21fc2c627eb8159494cc
SamuelFreitasExe <sffreitassamuel@gmail.com>
2024-08-13 15:10:40 -0300

    pequenos ajustes na logo e pagina projetos

--- src/site/Components/Menu/logo.jsx
+++ src/site/Components/Menu/logo.jsx
@@ -4,7 +4,7 @@ function Logo(){
     return (
         <div className='container' style={{ maxWidth: '250px' }}>
          <a className="navbar-brand" href="/#">
-          <img src="/Images/logo-trans.png" alt="Logo" style={{ maxHeight: '150px'}} />
+          <img src="/Images/logo3.png" alt="Logo" style={{ maxHeight: '75px'}} />
         </a>
         </div>
       );
--- src/site/Components/Projetos/projetos.jsx
+++ src/site/Components/Projetos/projetos.jsx
@@ -2,32 +2,36 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom'; // Importa useNavigate para navegação
 import './projeto.css'; // Certifique-se de que o caminho para o CSS está correto
 
+import img1 from './imgProjetos/img1.jpg';
+import img2 from './imgProjetos/img2.jpg';
+import img3 from './imgProjetos/img3.jpg';
+import img4 from './imgProjetos/img4.jpg';
+
 function Projetos() {
   const navigate = useNavigate(); // Cria uma instância de navigate
 
   const projetos = [
     {
-      imgSrc: '/imgProjetos/educacao.jpg',
+      imgSrc: img1,
       title: 'Educação para Todos',
       description: 'Oferecemos aulas de reforço escolar e atividades extracurriculares para garantir que cada criança tenha acesso à educação de qualidade.',
     },
     {
-      imgSrc: '/imgProjetos/esporte.jpg',
+      imgSrc: img2,
       title: 'Esporte e Cidadania',
       description: 'Promovemos atividades esportivas que incentivam o trabalho em equipe, disciplina e a saúde física e mental.',
     },
     {
-      imgSrc: '/imgProjetos/arte.jpg',
+      imgSrc: img3,
       title: 'Arte e Cultura',
       description: 'Organizamos oficinas de arte e eventos culturais que estimulam a criatividade e a expressão artística.',
     },
     {
-      imgSrc: '/imgProjetos/saude.jpg',
+      imgSrc: img4,
       title: 'Saúde e Bem-Estar',
       description: 'Realizamos campanhas de conscientização e atividades de promoção da saúde para a comunidade.',
     },
   ];
-
 
   const navegarParaPaginaFutebol = () => {
     navigate('/pagina-futebol'); // Navega para a página desejada
@@ -35,20 +39,17 @@ function Projetos() {
 
   return (
     <section id="projetos" className="projetos-section">
-      <div className="header">
-
+      <header className="header">
         <h2>Nossos Projetos</h2>
-      </div>
-      <div className="">
+      </header>
+      <div className="intro">
         <p>Conheça os projetos que desenvolvemos para impactar positivamente a vida das pessoas na comunidade de Foz do Iguaçu.</p>
-
       </div>
-
       <div className="projetos-list-container">
         <ul className="projetos-list">
           {projetos.map((projeto, index) => (
             <li key={index} className="projeto-item">
-              <img src="imgProjetos/img1.jpg" alt="Descrição da Imagem" />
+              <img src={projeto.imgSrc} alt={projeto.title} className="projeto-img" />
               <div className="projeto-content">
                 <h3>{projeto.title}</h3>
                 <p>{projeto.description}</p>
