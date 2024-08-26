8b7b4b2713b52b0f9b119f08e5cdd9a9d877ab1e
SamuelFreitasExe <sffreitassamuel@gmail.com>
2024-08-26 19:21:44 -0300

    atualizando footer

--- src/site/Components/Footer/footer.jsx
+++ src/site/Components/Footer/footer.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 
+function Footer() {
+  const ano = new Date().getFullYear();
 
-function Footer(){
-  var ano = new Date().getFullYear(); 
-
-    return <section id="footer">    
+  return (
+    <section id="footer">    
       <div>
-            <ul className="list-unstyled list-inline social text-center">
-                <li className="list-inline-item"><a href="/#"><i className="fa fa-facebook fa-2x"></i></a></li>
-                <li className="list-inline-item"><a href="/#"><i className="fa fa-twitter fa-2x"></i></a></li>
-                            
-                <li className="list-inline-item"><a href="/#"><i className="fa fa-instagram fa-2x"></i></a></li>						
-                <li className="list-inline-item"><a href="/#" target="_blank"><i className="fa fa-envelope fa-2x"></i></a></li>
-            </ul>
-	    </div>
-      <a href="images/Logos-01-1.png">Projeto teste</a>
+        <ul className="list-unstyled list-inline social text-center">
+          <li className="list-inline-item">
+            <a href="https://www.facebook.com/UniaoAlexandrense/" target="_blank" rel="noreferrer">
+              <i className="fa fa-facebook fa-2x"></i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a href="https://twitter.com" target="_blank" rel="noreferrer">
+              <i className="fa fa-twitter fa-2x"></i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a href="https://www.instagram.com/uniaoalexandrense/" target="_blank" rel="noreferrer">
+              <i className="fa fa-instagram fa-2x"></i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a href="mailto:uniaoalexandrense@gmail.com" target="_blank" rel="noreferrer">
+              <i className="fa fa-envelope fa-2x"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <a href="images/Logos-01-1.png" download>Projeto teste</a>
       <p>Samuel - {ano}</p>
-
-    </section>;
-  }
+    </section>
+  );
+}
 
 export default Footer;
