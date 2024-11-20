d5b8953bf55032032be8a56dc3606af96c953465
SamuelFreitasExe <sffreitassamuel@gmail.com>
2024-11-20 13:26:12 -0300

    ajuste nas fontes

--- src/site/Components/Sobre/sobre.jsx
+++ src/site/Components/Sobre/sobre.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function Sobre(){
     return <section id="sobre">
 
-  <div class="banner-content">
+  <div className="banner-content">
     <h2>Título do Banner</h2>
     <p>Mensagem principal ou descrição do projeto.</p>
-    <a href="/#" class="cta-button">Saiba Mais</a>
+    <a href="/#" className="cta-button">Saiba Mais</a>
   </div>
     </section>
   }
